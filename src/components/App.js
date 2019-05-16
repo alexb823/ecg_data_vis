61a877c92d6afad99af68d7ebd0824ea6548362a
Alex Bukhman <alexb823@gmail.com>
2019-05-16 17:27:29 -0400

    firts try at chart. using hard coded link

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {parseListOfDevises} from './utils';
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryZoomContainer,
+  VictoryBrushContainer,
+  VictoryAxis,
+} from 'victory';
+import { parseSmoothECG } from './utils';
 
 const App = () => {
-  axios
-    .get('/wxapp2/ecgdata/liveecg')
-    .then(response => parseListOfDevises(response.data))
-    .then(arr => console.log(arr));
-  return <div>Hello</div>;
+  const baseUrl = '/wxapp2/ecgdata/liveecg/5C0347004129';
+  let time = Date.parse('Thu, 16 May 2019 06:57:18 GMT');
+  let timeStamp = Date.parse('Thu, 16 May 2019 06:57:18 GMT');
+  const [ecgData, setEcgData] = useState([]);
+  const [zoomDomain, setZoomDomain] = useState({ x: [time, time + 6000] });
+  const [selectDomain, setSelectDomain] = useState({ x: [time, time + 6000] });
+
+  const handleZoom = domain => {
+    setSelectDomain(domain);
+  };
+
+  const handleBrush = domain => {
+    setZoomDomain(domain);
+  };
+
+  useEffect(() => {
+    axios
+    .get(`${baseUrl}/20190516/20190516_145335_5C0347004129_smoothECG.txt`)
+    .then(response => parseSmoothECG(response.data))
+    .then(ecg =>
+      ecg.map(sample => {
+        const dataPoint = { x: timeStamp, y: sample };
+        timeStamp += 4;
+        return dataPoint;
+      })
+    )
+    .then(dataPoints => setEcgData(dataPoints))
+    .then(() => console.log(ecgData));
+  }, [])
+
+  return (
+    <div>
+      <VictoryChart
+        width={600}
+        height={470}
+        scale={{ x: 'time' }}
+        containerComponent={
+          <VictoryZoomContainer
+            responsive={false}
+            zoomDimension="x"
+            zoomDomain={zoomDomain}
+            onZoomDomainChange={handleZoom}
+          />
+        }
+      >
+        <VictoryLine style={{ data: { stroke: 'tomato' } }} data={ecgData} />
+      </VictoryChart>
+
+      <VictoryChart
+        padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+        width={600}
+        height={100}
+        scale={{ x: 'time' }}
+        containerComponent={
+          <VictoryBrushContainer
+            responsive={false}
+            brushDimension="x"
+            brushDomain={selectDomain}
+            onBrushDomainChange={handleBrush}
+          />
+        }
+      >
+        <VictoryAxis tickFormat={x => new Date(x).getUTCMilliseconds()} />
+        <VictoryLine
+          style={{data: { stroke: 'tomato' }}}
+          data={ecgData}
+        />
+      </VictoryChart>
+    </div>
+  );
 };
 
 export default App;
--- src/components/utils.js
+++ src/components/utils.js
@@ -2,8 +2,25 @@ const parser = new DOMParser();
 
 //get the list of devise ids from the html at /wxapp2/ecgdata/liveecg
 export const parseListOfDevises = str => {
-  const nodeList = parser
+  const DeviseNodeList = parser
     .parseFromString(str, 'text/html')
     .querySelectorAll('a > tt');
-  return Array.from(nodeList).map(tt => tt.innerText.slice(0, -1));
+  return Array.from(DeviseNodeList).map(tt => tt.innerText.slice(0, -1));
+};
+
+// parse the smoothECG.txt file into an array of Int
+export const parseSmoothECG = str => {
+  const ecgNode = parser
+    .parseFromString(str, 'text/html')
+    .querySelector('body');
+  return ecgNode.innerText
+    .trim()
+    .split('\n')
+    .map(strNum => parseInt(strNum));
+  // console.log(
+  //   ecgNode.innerText
+  //     .trim()
+  //     .split('\n')
+  //     .map(strNum => parseInt(strNum))
+  // );
 };
