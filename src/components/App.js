997f87a232afb20c458d064e8d34ef571f8ba3d3
Alex Bukhman <alexb823@gmail.com>
2019-05-17 20:27:56 -0400

    creater several util function for scraping the data from the server

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {fetchModifiedDates, fetchDateStr, mapDates} from './utils'
+import {mapDatesAndFolders, mapDatesAndFiles} from './utils'
 import EcgGraph from './EcgGraph';
 
 const App = () => {
-  mapDates()
+  mapDatesAndFolders();
+  mapDatesAndFiles('20190506');
   return (
     <div>
       <EcgGraph/>
--- src/components/utils.js
+++ src/components/utils.js
@@ -11,25 +11,30 @@ export const parseListOfLinks = str => {
   return Array.from(nodeList).map(tt => tt.innerText.slice(0, -1));
 };
 
-// Parse list of GMT folder modified dates for the device at
+// Parse list of folder modified GMT dates for the device at
 export const parseModifiedDates = str => {
   const nodeList = parser
     .parseFromString(str, 'text/html')
     .querySelectorAll('td > tt');
   return [...nodeList]
     .map(tt => tt.innerText)
-    .filter(str => str.includes(' GMT'));
+    .filter(str => str.endsWith(' GMT'));
 };
 
-// Fetch the GMT folder modified dates for the device at /wxapp2/ecgdata/liveecg/:deviceId
-export const fetchModifiedDates = () => {
-  return axios
-  .get(`${baseUrl}`)
-  .then(response => parseModifiedDates(response.data))
-  .then(dates => dates.reverse());
+// Fetch the folder modified GMT dates for the device at /wxapp2/ecgdata/liveecg/:deviceId
+// and parse the mod date as we filter for only unique date values
+export const fetchModifiedDates = (folderName = '') => {
+  return (
+    axios
+      .get(`${baseUrl}/${folderName}`)
+      .then(response => parseModifiedDates(response.data))
+      // .then(dates => [...new Set(dates)])
+      .then(dates => dates.reverse())
+  );
 };
 
-// Fetch the folder name date strings for the device at /wxapp2/ecgdata/liveecg/:deviceId
+// Fetch the folder name (date strings) for the device at /wxapp2/ecgdata/liveecg/:deviceId
+// and parse the list of links
 export const fetchDateStr = () => {
   return axios
     .get(`${baseUrl}`)
@@ -38,16 +43,50 @@ export const fetchDateStr = () => {
 };
 
 // Make an array of obj for creating a list of links
-// Folder name to append to the base Url and date modified for the link text
-export const mapDates = () => {
+// Will have folder name to append to the baseUrl, and date modified for the link text
+export const mapDatesAndFolders = () => {
   return Promise.all([fetchDateStr(), fetchModifiedDates()])
-  .then(
-    ([dateStrArr, dateModArr]) =>
+    .then(([dateStrArr, dateModArr]) =>
       dateStrArr.map((date, idx) => {
         return { link: date, modDate: dateModArr[idx] };
       })
-  )
-  .then(dateMap => console.log(dateMap))
+    )
+    .then(dateMap => console.log(dateMap));
+};
+
+// Parse the data files in the date folder
+export const parseListOfFiles = str => {
+  const nodeList = parser
+    .parseFromString(str, 'text/html')
+    .querySelectorAll('a > tt');
+  return Array.from(nodeList).map(tt => tt.innerText);
+};
+
+// Fetch the data files for a date for the device at /wxapp2/ecgdata/liveecg/:deviceId/:folderNum
+// and parse the list of links
+export const fetchDataFile = folderName => {
+  return axios
+    .get(`${baseUrl}/${folderName}`)
+    .then(response => parseListOfFiles(response.data))
+    .then(files => files.reverse());
+};
+
+// Make an array of obj for creating a list of links for files
+// Will have file name to append to the baseUrl/folderName, to get the file,
+// and date modified (date needed for the graph)
+export const mapDatesAndFiles = folderName => {
+  return Promise.all([
+    fetchDataFile(folderName),
+    fetchModifiedDates(folderName),
+  ])
+    .then(([filesArr, dateModArr]) =>
+      dateModArr.reduce((acc, date, index) => {
+        if (acc[date]) acc[date].push(filesArr[index]);
+        else acc[date] = [filesArr[index]];
+        return acc;
+      }, {})
+    )
+    .then(dateMap => console.log(dateMap));
 };
 
 // parse the smoothECG.txt file into an array of Int
