1ef6056bc3300dc44eb2feb2084a9252c46d526f
Alex Bukhman <alexb823@gmail.com>
2019-05-22 22:41:47 -0400

    added redux devtools

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from 'redux-logger';
 import RootReducer from './reducers';
 
 const store = createStore(
   RootReducer,
-  applyMiddleware(thunkMiddleware)
+  composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
 );
 
 export default store;
