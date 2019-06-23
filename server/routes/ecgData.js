b9245dc2dada3098ac2edcf74d5967fef2bbd4c6
Alex Bukhman <alexb823@gmail.com>
2019-06-23 12:09:21 -0400

    secure true

--- server/routes/ecgData.js
+++ server/routes/ecgData.js
@@ -6,7 +6,7 @@ const proxy = require('http-proxy-middleware');
 const options = {
   target: 'https://paf3.ecordum.cn',
   auth: process.env.ECGDATA_AUTH,
-  secure: false,
+  secure: true,
 };
 
 const ecgDataProxy = proxy(options);
